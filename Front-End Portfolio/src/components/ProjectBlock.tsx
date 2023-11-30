cb581cbe1c0565f5dd5711b05d591c1d09b53e26
Uncle Pedro <you@example.com>
2023-11-30 16:44:36 +1000

    Create arrow element component and refine existing

--- Front-End Portfolio/src/components/ProjectBlock.tsx
+++ Front-End Portfolio/src/components/ProjectBlock.tsx
@@ -1,19 +1,16 @@
 function ProjectBlock(props: { title: string; description: string; img: string; isImgOnLeft: boolean }) {
   return (
     <>
-      <div className={'flex justify-center mt-32' + (props.isImgOnLeft ? ' flex-row-reverse' : '')}>
+      <div className={'flex justify-center mt-24' + (props.isImgOnLeft ? ' flex-row-reverse' : '')}>
         <div className={'w-[550px]' + (props.isImgOnLeft ? ' ml-16' : '')}>
           <h2 className="text-5xl font-semibold">{props.title}</h2>
           <p className={'text-2xl my-8'}>{props.description}</p>
         </div>
-        <img src={props.img} alt="headshot" className={'flex flex-end h-[300px] border-2 border-amber-400'} />
+        <div className={'flex flex-col'}>
+          <img src={props.img} alt="headshot" className={'flex flex-end h-[300px] border-2 border-amber-400'} />
+          <hr className={'flex justify-center border-t border-4 mt-24 border-amber-400 w-[540px]'} />
+        </div>
       </div>
-      <hr
-        className={
-          'flex justify-center border-t border-4 mt-24 border-amber-400 w-[540px]' +
-          (props.isImgOnLeft ? ' ml-[16.5%]' : ' ml-[50.3%]')
-        }
-      />
     </>
   );
 }
