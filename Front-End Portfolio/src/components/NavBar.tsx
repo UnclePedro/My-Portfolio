5480a00dcf27e591420f95b6afc4a2c653dd307f
Uncle Pedro <peterforsyth10@gmail.com>
2024-02-27 22:27:47 +1000

    Adjusting styling for project blocks

--- Front-End Portfolio/src/components/NavBar.tsx
+++ Front-End Portfolio/src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 function NavBar(props: { heading1: string; heading2: string; heading3: string }) {
   return (
-    <div className="justify-between p-8 mr-4">
+    <div className="justify-between items-center p-8 mr-4">
       <img src="/src/assets/favicon-p3.png" className="w-24" alt="P favicon" />
       <div className={'text-2xl font-poppins flex flex-row w-full space-x-4 justify-end'}>
-        <p>{props.heading1}</p>
-        <p>{props.heading2}</p>
-        <p>{props.heading3}</p>
+        <button>{props.heading1}</button>
+        <button>{props.heading2}</button>
+        <button>{props.heading3}</button>
       </div>
     </div>
   );
--- Front-End Portfolio/src/components/ProjectBlock.tsx
+++ Front-End Portfolio/src/components/ProjectBlock.tsx
@@ -7,19 +7,24 @@ function ProjectBlock(props: {
 }) {
   return (
     <>
-      <div className={'font-poppins flex justify-center mt-24' + (props.isImgOnLeft ? ' flex-row-reverse' : '')}>
-        <div className={'w-[550px]' + (props.isImgOnLeft ? ' ml-16' : '')}>
+      <div
+        className={
+          'font-poppins flex justify-center mt-24 bg-black p-10 rounded-lg' +
+          (props.isImgOnLeft ? ' flex-row-reverse' : '')
+        }
+      >
+        <div className={'max-w-[550px]' + (props.isImgOnLeft ? ' ml-16' : '')}>
           <h2 className="text-5xl font-semibold">{props.title}</h2>
           <p className={'text-2xl my-8'}>{props.description}</p>
         </div>
         <div className={'flex flex-col'}>
           <img src={props.img} alt="headshot" className={'flex flex-end h-[300px] border-2 border-amber-400'} />
-          <hr
+          {/* <hr
             className={
               'flex justify-center border-t border-4 mt-24 border-amber-400 w-[540px]' +
               (props.displayLine ? '' : ' hidden')
             }
-          />
+          /> */}
         </div>
       </div>
     </>
