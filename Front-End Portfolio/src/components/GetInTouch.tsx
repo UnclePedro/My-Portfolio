13a49e59d00819972f2276cfcac77aa29449bbe9
Uncle Pedro <peterforsyth10@gmail.com>
2024-02-29 09:51:35 +1000

    Finesse nav bar and styling of other components

--- Front-End Portfolio/src/components/GetInTouch.tsx
+++ Front-End Portfolio/src/components/GetInTouch.tsx
@@ -4,7 +4,7 @@ function GetInTouch(props: { title: string; bodyText: string; buttonText: string
       <div className={'w-[650px] flex-col align-center text-center'}>
         <h2 className="text-6xl font-semibold">{props.title}</h2>
         <p className={'text-2xl my-8'}>{props.bodyText}</p>
-        <button className={' bg-black border-[2px] border-yellow-300 h-14 w-32 rounded-md font-semibold'}>
+        <button className={' bg-yellow-300 text-gray-800 h-14 w-32 rounded-md font-semibold'}>
           {props.buttonText}
         </button>
       </div>
