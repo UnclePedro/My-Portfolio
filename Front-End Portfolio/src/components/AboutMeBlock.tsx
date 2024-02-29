a12ebdd1b5f9ae1374b2bcaaeb9d6191c55ebdb2
Uncle Pedro <you@example.com>
2024-02-29 11:52:49 +1000

    Adjust colors and styling of project block etc

--- Front-End Portfolio/src/components/AboutMeBlock.tsx
+++ Front-End Portfolio/src/components/AboutMeBlock.tsx
@@ -4,7 +4,7 @@ function AboutMeBlock(props: { title: string; description1: string; description2
       <div className={'font-poppins flex justify-center'}>
         <div className={'w-[650px]'}>
           <h2 className="text-6xl font-semibold">
-            <span className={'text-yellow-300 font-extrabold'}>&gt; </span>
+            <span className={'text-amber-400 font-extrabold'}>&gt; </span>
             {props.title}
           </h2>
           <p className={'text-2xl my-8'}>{props.description1}</p>
@@ -14,24 +14,24 @@ function AboutMeBlock(props: { title: string; description1: string; description2
           <div className={'flex'}>
             <ol className={'text-2xl my-8'}>
               <li>
-                <span className={'text-yellow-300 font-extrabold'}>&gt;</span> JavaScript
+                <span className={'text-amber-400 font-extrabold'}>&gt;</span> JavaScript
               </li>
               <li>
-                <span className={'text-yellow-300 font-extrabold'}>&gt;</span> React
+                <span className={'text-amber-400 font-extrabold'}>&gt;</span> React
               </li>
               <li>
-                <span className={'text-yellow-300 font-extrabold'}>&gt;</span> CSS
+                <span className={'text-amber-400 font-extrabold'}>&gt;</span> CSS
               </li>
             </ol>
             <ol className={'text-2xl my-8 inline-flex flex-col pl-20'}>
               <li>
-                <span className={'text-yellow-300 font-extrabold'}>&gt;</span> TypeScript
+                <span className={'text-amber-400 font-extrabold'}>&gt;</span> TypeScript
               </li>
               <li>
-                <span className={'text-yellow-300 font-extrabold'}>&gt;</span> Tailwind
+                <span className={'text-amber-400 font-extrabold'}>&gt;</span> Tailwind
               </li>
               <li>
-                <span className={'text-yellow-300 font-extrabold'}>&gt;</span> HTML
+                <span className={'text-amber-400 font-extrabold'}>&gt;</span> HTML
               </li>
             </ol>
           </div>
