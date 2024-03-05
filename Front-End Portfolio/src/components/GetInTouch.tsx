9be86bbf1d3f6b023c132622b25bdb282958b4ae
Uncle Pedro <peterforsyth10@gmail.com>
2024-03-05 22:02:41 +1000

    Create video wall component - Adjust shadow on project block and about portrait - Adjust contact block

--- Front-End Portfolio/src/components/GetInTouch.tsx
+++ Front-End Portfolio/src/components/GetInTouch.tsx
@@ -1,10 +1,12 @@
-function GetInTouch(props: { title: string; bodyText: string; buttonText: string }) {
+function GetInTouch(props: { title: string; bodyText: string; buttonText: string; phone: string; email: string }) {
   return (
     <div className={'font-poppins flex justify-center pt-32'} id="contact">
       <div className={'w-[650px] flex-col align-center text-center'}>
         <h2 className="text-6xl font-semibold">{props.title}</h2>
         <p className={'text-2xl my-8'}>{props.bodyText}</p>
-        <button className={' bg-amber-400 text-gray-800 h-14 w-32 rounded-md font-semibold'}>{props.buttonText}</button>
+        <p className={'text-2xl font-semibold my-8'}>{props.email}</p>
+        <p className={'text-2xl my-8 font-semibold'}>{props.phone}</p>
+        {/* <button className={' bg-amber-400 text-gray-800 h-14 w-32 rounded-md font-semibold'}>{props.buttonText}</button> */}
       </div>
     </div>
   );
