c6df3d4001d45dbaf79c3fe43098e0bf56405816
Uncle Pedro <you@example.com>
2024-03-06 09:26:13 +1000

    Tinker with video player import issue

--- Front-End Portfolio/src/components/VideoWall.tsx
+++ Front-End Portfolio/src/components/VideoWall.tsx
@@ -4,10 +4,10 @@ function VideoWall(props: { url3: string; url2: string; url1: string }) {
   return (
     <>
       <div className={'mt-6'}>
-        <VideoPlayer url={props.url1} width={'750px'} height={'422px'} />
+        <VideoPlayer url={props.url1} width={'700px'} height={'394px'} />
         <div className={'flex justify-center'}>
-          <VideoPlayer url={props.url2} width={'640px'} height={'360px'} />
-          <VideoPlayer url={props.url3} width={'640px'} height={'360px'} />
+          <VideoPlayer url={props.url2} width={'550px'} height={'309px'} />
+          <VideoPlayer url={props.url3} width={'550px'} height={'309px'} />
         </div>
       </div>
     </>
