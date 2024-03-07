1eb911d8cb2356a4c7386b4435d62e5b7f2ef248
Uncle Pedro <peterforsyth10@gmail.com>
2024-03-07 17:57:24 +1000

    Create icon component for contact block - Import SVG's for icons

--- Front-End Portfolio/src/components/GetInTouch.tsx
+++ Front-End Portfolio/src/components/GetInTouch.tsx
@@ -1,11 +1,21 @@
-function GetInTouch(props: { title: string; bodyText: string; buttonText: string; phone: string; email: string }) {
+import Icon from './Icon';
+import linkedin from '../../src/assets/icons/linkedin.svg';
+import instagram from '../../src/assets/icons/instagram.svg';
+// import email from '../../src/assets/icons/email.svg';
+import githubIcon from '../../src/assets/icons/github.svg';
+
+function GetInTouch(props: { title: string; bodyText: string; buttonText: string }) {
   return (
-    <div className={'font-poppins flex justify-center pt-32'}>
+    <div className={'font-poppins flex justify-center pt-48'}>
       <div className={'w-[650px] flex-col align-center text-center'}>
         <h2 className="text-6xl font-semibold">{props.title}</h2>
         <p className={'text-2xl my-8'}>{props.bodyText}</p>
-        <p className={'text-2xl font-semibold my-8'}>{props.email}</p>
-        <p className={'text-2xl my-8 font-semibold'}>{props.phone}</p>
+        <div className={'flex justify-center'}>
+          <Icon img={linkedin} href={'https://www.linkedin.com/in/peter-forsyth-a89590175/'} alt={'linkedIn'} />
+          <Icon img={instagram} href={'https://www.instagram.com/peterforsyth_/'} alt={'Instagram'} />
+          {/* <Icon img={email} href={'https://www.instagram.com/peterforsyth_/'} alt={'email'} /> */}
+          <Icon img={githubIcon} href={'https://github.com/UnclePedro'} alt={'Github'} />
+        </div>
         {/* <button className={' bg-amber-400 text-gray-800 h-14 w-32 rounded-md font-semibold'}>{props.buttonText}</button> */}
       </div>
     </div>
--- Front-End Portfolio/src/components/VideoWall.tsx
+++ Front-End Portfolio/src/components/VideoWall.tsx
@@ -4,10 +4,10 @@ function VideoWall(props: { url3: string; url2: string; url1: string }) {
   return (
     <>
       <div className={'mt-6'}>
-        <VideoPlayer url={props.url1} width={'700px'} height={'394px'} />
+        <VideoPlayer url={props.url1} width={'650px'} height={'366px'} />
         <div className={'flex justify-center'}>
-          <VideoPlayer url={props.url2} width={'550px'} height={'309px'} />
-          <VideoPlayer url={props.url3} width={'550px'} height={'309px'} />
+          <VideoPlayer url={props.url2} width={'500px'} height={'281px'} />
+          <VideoPlayer url={props.url3} width={'500px'} height={'281px'} />
         </div>
       </div>
     </>
