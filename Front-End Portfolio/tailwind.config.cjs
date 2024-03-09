f50998a3c56bacd2cb4b5394ef4c4012ea101b4f
Uncle Pedro <peterforsyth10@gmail.com>
2024-03-09 12:13:41 +1000

    Create custom Tailwind shadow themes

--- Front-End Portfolio/tailwind.config.cjs
+++ Front-End Portfolio/tailwind.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
+      boxShadow: {
+        goldenSmall: '0 0 4px rgba(251, 191, 36, .2), 0 0 20px rgba(251, 191, 36, .4)',
+        goldenMedium: '0 0 13px rgba(251, 191, 36, .5), 0 0 30px rgba(251, 191, 36, .4)',
+      },
     },
   },
   plugins: [],
