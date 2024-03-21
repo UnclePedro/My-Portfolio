67a09ce90cb95298a7518e1b7f04d3cb16222f96
Uncle Pedro <peterforsyth10@gmail.com>
2024-03-21 17:18:39 +1000

    Add scroll event listener to toggle hamburger - Change P favicon colour

--- Front-End Portfolio/src/components/NavBar.tsx
+++ Front-End Portfolio/src/components/NavBar.tsx
@@ -42,7 +42,15 @@ function NavBar(props: {
     };
   });
 
-  // hamburger menu logic and function. This is called in scrollToSection to hide hamburger on button tap
+  // Close hamburger on scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      setHamburgerOpen(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+  });
+
+  // Hamburger menu logic and function. This is called in scrollToSection to hide hamburger on button selection
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
 
   const toggleHamburger = () => {
@@ -50,19 +58,19 @@ function NavBar(props: {
   };
 
   return (
-    <div className="flex w-full p-8 absolute">
+    <div className="flex w-full p-8 absolute" onScroll={toggleHamburger}>
       <div className={'flex justify-between w-full'}>
         <img src="/src/assets/favicon-p.png" className="w-16 md:w-24 animation-reverse-bounce" alt="P favicon" />
         <button onClick={toggleHamburger}>
           <img
             src="/src/assets/icons/hamburger.svg"
-            className="justify-end right-8 top-10 fixed w-12 bg-neutral-800 rounded-lg p-2 md:hidden"
+            className="justify-end right-8 top-10 fixed w-12 bg-neutral-800 rounded-xl p-2 md:hidden"
             alt="Hamburger"
           />
         </button>
       </div>
       {hamburgerOpen && (
-        <div className={'flex flex-col text-left fixed text-2xl font-poppins bg-neutral-700 rounded-xl p-4 md:hidden'}>
+        <div className={`flex flex-col fixed text-xl font-poppins bg-neutral-800 rounded-xl p-4 md:hidden`}>
           <button
             onClick={() => scrollToSection(props.aboutRef)}
             className="transition ease-in-out duration-500 hover:text-amber-400"
@@ -71,31 +79,31 @@ function NavBar(props: {
           </button>
           <button
             onClick={() => scrollToSection(props.projectRef)}
-            className="transition ease-in-out duration-500 hover:text-amber-400"
+            className="transition ease-in-out duration-500 hover:text-amber-400 pt-2"
           >
             {props.heading2}
           </button>
           <button
             onClick={() => scrollToSection(props.creativeRef)}
-            className="transition ease-in-out duration-500 hover:text-amber-400"
+            className="transition ease-in-out duration-500 hover:text-amber-400 pt-2"
           >
             {props.heading3}
           </button>
           <button
             onClick={() => scrollToSection(props.contactRef)}
-            className="transition ease-in-out duration-500 hover:text-amber-400"
+            className="transition ease-in-out duration-500 hover:text-amber-400 pt-2"
           >
             {props.heading4}
           </button>
 
-          {showBackToTop && (
+          {/* {showBackToTop && (
             <button
               className="transition ease-in-out duration-500 hover:text-amber-400 ${showBackToTop ? 'opacity-100' : 'opacity-0 hidden'"
               onClick={() => scrollToSection(props.landingBannerRef)}
             >
               Back to top
             </button>
-          )}
+          )} */}
         </div>
       )}
 
