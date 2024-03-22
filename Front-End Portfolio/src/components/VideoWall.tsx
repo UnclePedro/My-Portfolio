09abe5336b1c3fd3f82aca727489c1dd57973de8
Uncle Pedro <you@example.com>
2024-03-22 10:18:15 +1000

    Tinkering with video player responsiveness

--- Front-End Portfolio/src/components/VideoWall.tsx
+++ Front-End Portfolio/src/components/VideoWall.tsx
@@ -3,7 +3,7 @@ import VideoPlayer from './VideoPlayer';
 function VideoWall(props: { url3: string; url2: string; url1: string }) {
   return (
     <>
-      <div className={'my-12'}>
+      <div className={'sm:my-8'}>
         <VideoPlayer url={props.url1} width={'500px'} height={'281px'} />
         <div className={'flex flex-col xl:flex-row justify-center'}>
           <VideoPlayer url={props.url2} width={'500px'} height={'281px'} />
