4f64881d04c4f594e0a705abdd9e43f34bd8a8a4
Uncle Pedro <peterforsyth10@gmail.com>
2024-03-22 10:18:15 +1000

    Tinkering with video player responsiveness

--- Front-End Portfolio/src/components/VideoPlayer.tsx
+++ Front-End Portfolio/src/components/VideoPlayer.tsx
@@ -5,7 +5,9 @@ const ReactPlayer = _ReactPlayer as unknown as React.FC<ReactPlayerProps>;
 function VideoPlayer(props: { url: string; width: string; height: string }) {
   return (
     <>
-      <div className={'m-4 font-poppins flex justify-center'}>
+      <div className={'mt-4 px-8 flex flex-col justify-center'}>
+        <ReactPlayer url={props.url} width={props.width} height={props.height} controls />
+        <ReactPlayer url={props.url} width={props.width} height={props.height} controls />
         <ReactPlayer url={props.url} width={props.width} height={props.height} controls />
       </div>
     </>
