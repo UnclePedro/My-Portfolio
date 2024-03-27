499a839983c32806523f4289872b2d38b1a18144
Uncle Pedro <peterforsyth10@gmail.com>
2024-03-27 18:45:06 +1000

    Fix import error

--- Front-End Portfolio/src/App.tsx
+++ Front-End Portfolio/src/App.tsx
@@ -1,6 +1,6 @@
 //import { useState } from 'react';
 //import styles from './App.module.css';
-import headshot from '../src/assets/headshot-assets/headshot-square.png';
+import headshot from '../src/assets/headshot-assets/Headshot-square.png';
 import tempProjectImg from '../src/assets/project-placeholder.png';
 import playThisProjectImg from '../src/assets/play-this-project.png';
 import ProjectBlock from './components/ProjectBlock';
