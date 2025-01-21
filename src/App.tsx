b83365aeac7a24ed4a81587e1aef0d5f8341780d
Uncle Pedro <peterforsyth10@gmail.com>
2025-01-22 09:58:13 +1000

    Adjust copy - Tampered with incorrect folder structure, not worth the hassle

--- src/App.tsx
+++ src/App.tsx
@@ -61,13 +61,13 @@ function App() {
 
         <ProjectBlock
           description={
-            'Keep track of your climbing training, goals and achievements across seasons in one place. Data analysis features are under development.'
+            'Keep track of your climbing training, goals and achievements across seasons in one place. Create and account or log in seamlessly with incling Google, Apple and Microsoft.'
           }
           title={'Climb Harder'}
           img={climbHarderProjectImg}
           isImgOnLeft={false}
           projectLink={'https://climb-harder.peterforsyth.dev'}
-          githubLink={'https://github.com/UnclePedro/Climb-Harder'}
+          githubLink={'https://github.com/UnclePedro/Climb-Harder-v2'}
         />
 
         <ProjectBlock
